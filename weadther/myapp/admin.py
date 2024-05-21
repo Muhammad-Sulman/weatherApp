8c898e3827a8055e315f488a9dd21caed04e53c4
Muhammad Suleman <148377324+Muhammad-Sulman@users.noreply.github.com>
2024-05-21 09:35:06 +0500

    Add files via upload

---
+++ weadther/myapp/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import City
+# Register your models here. username m.suleman password Numl2805 , email = sulemanahmed643@gmail.com
+admin.site.register(City)
